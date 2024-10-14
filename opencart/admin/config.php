0ac20c291486afb7fd6f63ab19623b2e45e42196
PavelShevchenko1 <pavelshevchen.ko107@yandex.ru>
2024-10-14 16:00:07 +0500

    config files 2

--- opencart/admin/config.php
+++ opencart/admin/config.php
@@ -1,18 +1,18 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://kenek.local/admin/');
-define('HTTP_CATALOG', 'http://kenek.local/');
+define('HTTP_SERVER', 'http://tmkenek.com.tm/admin/');
+define('HTTP_CATALOG', 'http://tmkenek.com.tm/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://kenek.local/admin/');
-define('HTTPS_CATALOG', 'http://kenek.local/');
+define('HTTPS_SERVER', 'http://tmkenek.com.tm/admin/');
+define('HTTPS_CATALOG', 'http://tmkenek.com.tm/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/OSPanel/domains/kenekShop/opencart/admin/');
-define('DIR_SYSTEM', 'C:/OSPanel/domains/kenekShop/opencart/system/');
-define('DIR_IMAGE', 'C:/OSPanel/domains/kenekShop/opencart/image/');
-define('DIR_STORAGE', 'C:/OSPanel/domains/kenekShop/storage/');
-define('DIR_CATALOG', 'C:/OSPanel/domains/kenekShop/opencart/catalog/');
+define('DIR_APPLICATION', '/var/www/kenek/opencart/admin/');
+define('DIR_SYSTEM', '/var/www/kenek/opencart/system/');
+define('DIR_IMAGE', '/var/www/kenek/opencart/image/');
+define('DIR_STORAGE', '/var/www/kenek/storage/');
+define('DIR_CATALOG', '/var/www/kenek/opencart/catalog/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -26,9 +26,9 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'kenek');
+define('DB_USERNAME', 'kenek_user');
+define('DB_PASSWORD', 'f4AgKo~%8v{OOt|W');
+define('DB_DATABASE', 'kenek_db');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
